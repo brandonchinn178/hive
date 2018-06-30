8696d3cf63cc29dbe7e423bc38fbf7ac74b8e998
Brandon Chinn <brandonchinn178@gmail.com>
2018-06-29 18:26:27 -0700

    Add getNeighbors

--- src/Hive/Coordinate.hs
+++ src/Hive/Coordinate.hs
@@ -1,5 +1,6 @@
 module Hive.Coordinate
   ( Coordinate
+  , getNeighbors
   ) where
 
 -- | A coordinate on the Hive board.
@@ -19,3 +20,14 @@ module Hive.Coordinate
 -- The positive x-axis goes to the south east edge and the
 -- positive y-axis goes to the north.
 type Coordinate = (Int, Int)
+
+-- | Get the coordinates surrounding the given coordinate.
+getNeighbors :: Coordinate -> [Coordinate]
+getNeighbors (x, y) =
+  [ (x, y + 1)     -- N
+  , (x + 1, y + 1) -- NE
+  , (x + 1, y)     -- SE
+  , (x, y - 1)     -- S
+  , (x - 1, y - 1) -- SW
+  , (x - 1, y)     -- NW
+  ]
