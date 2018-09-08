0b754fd2a89928154e7aed248e645a17dabe11b9
Brandon Chinn <brandonchinn178@gmail.com>
2018-09-08 10:09:28 -0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 #
-# With no arguments, builds everything. Can also take in one or more
-# projects to build.
+# With no arguments, builds everything. Takes in the usual stack arguments,
+# except instead of the usual stack targets, the available stack targets
+# are: lib, server, client.
 
 set -eu -o pipefail
 
 builtin cd "$(dirname "${BASH_SOURCE[0]}")"
 
-USAGE='"
-Usage: $0 [PROJECT ..]
+ARGS=()
 
-PROJECT: lib, server, client
-"'
+BUILD_ALL=1
+BUILD_LIB=0
+BUILD_SERVER=0
+BUILD_CLIENT=0
 
-if [[ $# == 0 ]]; then
-    BUILD_LIB=1
-    BUILD_SERVER=1
-    BUILD_CLIENT=1
-else
-    BUILD_LIB=0
-    BUILD_SERVER=0
-    BUILD_CLIENT=0
-    while [[ $# -gt 0 ]]; do
-        BUILD_ALL=0
-        case $1 in
-            (lib)    BUILD_LIB=1 ;;
-            (server) BUILD_SERVER=1 ;;
-            (client) BUILD_CLIENT=1 ;;
-            (*)
-                eval echo "$USAGE" >&2
-                exit 1
-            ;;
-        esac
-        shift
-    done
-fi
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        (lib)    BUILD_ALL=0; BUILD_LIB=1 ;;
+        (server) BUILD_ALL=0; BUILD_SERVER=1 ;;
+        (client) BUILD_ALL=0; BUILD_CLIENT=1 ;;
+        (*)      ARGS+=("$1") ;;
+    esac
+    shift
+done
+
+BUILD_LIB=$(( BUILD_ALL || BUILD_LIB ))
+BUILD_SERVER=$(( BUILD_ALL || BUILD_SERVER ))
+BUILD_CLIENT=$(( BUILD_ALL || BUILD_CLIENT ))
 
 if (( BUILD_CLIENT )); then
-    stack build --stack-yaml stack-ghcjs.yaml hive-client
+    stack build --stack-yaml stack-ghcjs.yaml hive-client "${ARGS[@]}"
 
     CLIENT_ROOT=$(stack path --stack-yaml stack-ghcjs.yaml --local-install-root)
     mkdir -p server/static
@@ -52,5 +45,5 @@ if (( BUILD_LIB || BUILD_SERVER )); then
     if (( BUILD_SERVER )); then
         STACK_ARGS+=(hive-server)
     fi
-    stack build "${STACK_ARGS[@]}"
+    stack build "${STACK_ARGS[@]}" "${ARGS[@]}"
 fi
