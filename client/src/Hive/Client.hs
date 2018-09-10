51983179e097169a058311d6dba12658310a8c9e
Brandon Chinn <brandonchinn178@gmail.com>
2018-09-09 23:59:17 -0700

    Add state to client

--- client/src/Hive/Client.hs
+++ client/src/Hive/Client.hs
@@ -1,8 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Strict #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Hive.Client (app) where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Typeable (Typeable)
 import React.Flux
 
+import Hive.Core (Coordinate, HiveState, startState)
+
+data AppState = AppState
+  { hiveState  :: HiveState
+  , validMoves :: Set Coordinate
+  } deriving (Show,Typeable)
+
+data AppAction
+  = PutPiece
+  | ShowValidMoves
+  | ClearValidMoves
+  deriving (Show,Typeable)
+
+instance StoreData AppState where
+  type StoreAction AppState = AppAction
+  transform = undefined
+
+appState :: ReactStore AppState
+appState = mkStore $ AppState
+  { hiveState = startState
+  , validMoves = Set.empty
+  }
+
 app :: ReactView ()
-app = defineView "Main Hive app" $ \() -> return ()
+app = defineControllerView "Main Hive app" appState $ \state () -> return ()
