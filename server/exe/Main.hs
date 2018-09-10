46e406a1bf79e1829cfafc827aaa60376b22d426
Brandon Chinn <brandonchinn178@gmail.com>
2018-09-09 21:45:01 -0700

    Add backend server (#7)
    * Get working server
    
    * Add STATIC_DIR environment variable
    
    * Add zlib-devel

--- server/exe/Main.hs
+++ server/exe/Main.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
+import Network.Wai.Handler.Warp (run)
+import Path.IO (resolveDir')
+import System.Environment (lookupEnv)
+
+import Hive.Server
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = do
+  staticDir <- lookupEnv "STATIC_DIR" >>= \case
+    Just dir -> resolveDir' dir
+    Nothing -> fail "STATIC_DIR is not defined."
+  putStrLn "Running on port 3000."
+  run 3000 $ app ServerSettings{..}
--- stack.sh
+++ stack.sh
@@ -6,4 +6,6 @@ set -eu -o pipefail
 
 builtin cd "$(dirname "${BASH_SOURCE[0]}")"
 
+export STATIC_DIR=$PWD/server/static
+
 exec stack --stack-yaml stack.yaml "$@"
