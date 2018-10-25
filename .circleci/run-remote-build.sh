a881b714784e197b9087480fae8f3c76209396e0
Brandon Chinn <brandonchinn178@gmail.com>
2018-10-24 17:59:19 -0700

    Implement remote build

--- .circleci/run-remote-build.sh
+++ .circleci/run-remote-build.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-set -eu -o pipefail
+set -eux -o pipefail
 
-ls
+# unpack files
+tar xf repo.tar.gz
 
 if [[ -f ghcjs-cache.tar.gz ]]; then
-    tar tf ghcjs-cache.tar.gz
+    tar xf ghcjs-cache.tar.gz
+    mv usr-bin /usr/local/bin/
+    mv usr-lib /usr/local/lib/
+    rm ghcjs-cache.tar.gz
 fi
+
+sudo scripts/install-system-deps.sh
+scripts/install-stack-deps.sh
+stack build alex happy
+ghcjs/stack.sh setup
+ghcjs/stack.sh build --only-dependencies
+
+# pack cache
+mv /usr/local/bin/ usr-bin
+mv /usr/local/lib/ usr-lib
+tar czf ghcjs-cache.tar.gz \
+    .stack \
+    .ghcjs \
+    usr-bin \
+    usr-lib
+
+ghcjs/build.sh
+tar czf build.tar.gz build/
+
+tar czf everything.tar.gz build.tar.gz ghcjs-cache.tar.gz
