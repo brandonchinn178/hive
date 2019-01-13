d619f0f8c0b607434c2673b61b63729875650d39
Brandon Chinn <brandonchinn178@gmail.com>
2019-01-12 22:53:22 -0800

    Move renderPiece into Hive.Client.Piece

--- src/Hive/Client/Board.hs
+++ src/Hive/Client/Board.hs
@@ -1,73 +1,28 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Hive.Client.Board
   ( renderBoard
   ) where
 
 import qualified Data.Map.Strict as Map
-import Data.Monoid ((<>))
-import qualified Data.Text as Text
 import Reflex.Dom.Core
 
-import Hive.Client.Coordinate (asPoints, toSVGCoord)
-import Hive.Client.Piece
-import Hive.Client.SVG (svgAttr)
-import Hive.Core.Board (Board, PlayerPiece, coordinateMap)
-import Hive.Core.Coordinate (Coordinate)
-import Hive.Core.Piece (PieceType(..), pieceToType)
-import Hive.Core.Player (Player(..))
+import Hive.Client.Coordinate (toSVGCoord)
+import Hive.Client.Piece (renderPiece)
+import Hive.Core.Board (Board, coordinateMap)
 
 renderBoard :: MonadWidget t m => Board -> m ()
-renderBoard board = mapM_ (uncurry renderPiece) $ Map.toList $ normalize coordMap
+renderBoard board = mapM_ renderPiece' $ Map.toList $ normalize coordMap
   where
     coordMap = coordinateMap board
     coords = Map.keys coordMap
+    renderPiece' (coord, pieces) = case pieces of
+      [] -> fail $ "Coordinate has no pieces: " ++ show coord
+      piece:_ -> renderPiece (toSVGCoord coord) piece
     -- make Coordinate (0, 0) the top-left corner in the SVG
     leftMost = minimum $ map fst coords
     topMost = maximum $ map snd coords
     normalize = Map.mapKeys $ \(x,y) -> (x + leftMost, y - topMost)
-
-renderPiece :: MonadWidget t m => Coordinate -> [PlayerPiece] -> m ()
-renderPiece _ [] = error "Coordinate has no pieces"
-renderPiece coord ((player, piece):_) = svgAttr "g" attrs $ do
-  hexagon
-  case pieceToType piece of
-    BeeType -> svgBee
-    AntType -> svgAnt
-    GrasshopperType -> svgGrasshopper
-    BeetleType -> svgBeetle
-    SpiderType -> svgSpider
-  where
-    attrs = mconcat
-      [ "class" =: Text.unwords
-          [ "piece"
-          , case player of
-              One -> "player1"
-              Two -> "player2"
-          ]
-      , "transform" =: Text.unwords
-          [ "scale(0.2)" -- 500x500 -> 100x100
-          , "translate(" <> asPoints (toSVGCoord coord) <> ")"
-          ]
-      ]
-
-{- Helpers -}
-
--- | An SVG hexagon.
-hexagon :: MonadWidget t m => m ()
-hexagon = svgAttr "polygon" attrs $ pure ()
-  where
-    attrs = mconcat
-      [ "class" =: "hex"
-      , "points" =: Text.unwords (map asPoints hexagonPoints)
-      ]
-    -- regular hexagon, 500px high
-    hexagonPoints =
-      [ (0            , 250)
-      , (250  / sqrt 3, 0  )
-      , (750 / sqrt 3 , 0  )
-      , (1000 / sqrt 3, 250)
-      , (750 / sqrt 3 , 500)
-      , (250  / sqrt 3, 500)
-      ]
--- src/Hive/Client/Piece.hs
+++ src/Hive/Client/Piece.hs
@@ -5,20 +5,46 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Hive.Client.Piece
-  ( svgAnt
-  , svgBee
-  , svgBeetle
-  , svgGrasshopper
-  , svgSpider
+  ( renderPiece
   ) where
 
 import Data.Foldable (traverse_)
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Reflex.Dom.Core
 
+import Hive.Client.Coordinate (SVGCoordinate, asPoints)
 import Hive.Client.SVG (svgAttr)
 import Hive.Client.SVG.TH (loadSVG)
+import Hive.Core.Board (PlayerPiece)
+import Hive.Core.Piece (PieceType(..), pieceToType)
+import Hive.Core.Player (Player(..))
+
+renderPiece :: MonadWidget t m => SVGCoordinate -> PlayerPiece -> m ()
+renderPiece coord (player, piece) = svgAttr "g" attrs $ do
+  hexagon
+  case pieceToType piece of
+    BeeType -> svgBee
+    AntType -> svgAnt
+    GrasshopperType -> svgGrasshopper
+    BeetleType -> svgBeetle
+    SpiderType -> svgSpider
+  where
+    attrs = mconcat
+      [ "class" =: Text.unwords
+          [ "piece"
+          , case player of
+              One -> "player1"
+              Two -> "player2"
+          ]
+      , "transform" =: Text.unwords
+          [ "scale(0.2)" -- 500x500 -> 100x100
+          , "translate(" <> asPoints coord <> ")"
+          ]
+      ]
+
+{- Helpers -}
 
 toSVGPath :: MonadWidget t m => Text -> [String] -> m ()
 toSVGPath cls = svgAttr "g" attrs . traverse_ (fromPath . Text.pack)
@@ -43,3 +69,21 @@ svgGrasshopper = toSVGPath "grasshopper" $(loadSVG "grasshopper.svg")
 
 svgSpider :: MonadWidget t m => m ()
 svgSpider = toSVGPath "spider" $(loadSVG "spider.svg")
+
+-- | An SVG hexagon.
+hexagon :: MonadWidget t m => m ()
+hexagon = svgAttr "polygon" attrs $ pure ()
+  where
+    attrs = mconcat
+      [ "class" =: "hex"
+      , "points" =: Text.unwords (map asPoints hexagonPoints)
+      ]
+    -- regular hexagon, 500px high
+    hexagonPoints =
+      [ (0            , 250)
+      , (250  / sqrt 3, 0  )
+      , (750 / sqrt 3 , 0  )
+      , (1000 / sqrt 3, 250)
+      , (750 / sqrt 3 , 500)
+      , (250  / sqrt 3, 500)
+      ]
