7466214459f0b7b00569ba722d4e42c8c01f23a3
Brandon Chinn <brandonchinn178@gmail.com>
2019-01-25 23:01:22 -0800

    Initial commit for creating the start screen

--- src/Hive/Client.hs
+++ src/Hive/Client.hs
@@ -3,6 +3,7 @@
 
 module Hive.Client (app) where
 
+import Control.Monad (zipWithM)
 import qualified Data.Text as Text
 import Hasmin (minifyCSS)
 import Language.Haskell.TH.Syntax (addDependentFile, lift, runIO)
@@ -13,9 +14,11 @@ import System.FilePath ((</>))
 import System.Process (readProcess)
 
 import Hive.Client.Board (renderBoard)
-import Hive.Client.SVG (svgAttr)
+import Hive.Client.Coordinate (toSVGCoord)
+import Hive.Client.Piece (renderPiece)
+import Hive.Client.SVG (svg', svgAttr)
 import Hive.Core.Board (emptyBoard, putPiece)
-import Hive.Core.Piece (Piece(..))
+import Hive.Core.Piece (Piece(..), PieceType(..))
 import Hive.Core.Player (Player(..))
 
 app :: JSM ()
@@ -30,13 +33,22 @@ header = do
 
 body :: MonadWidget t m => m ()
 body = svgAttr "svg" ("viewBox" =: "0 0 1000 1000") $
-  renderBoard
-    $ putPiece (One, Bee) (0, -1)
-    . putPiece (Two, Grass0) (1, 2)
-    . putPiece (One, Beetle0) (1, 0)
-    . putPiece (Two, Spider0) (0, 1)
-    . putPiece (One, Ant0) (0, 0)
-    $ emptyBoard
+  -- TODO: make header with title and game info
+  if True -- TODO: make game loop
+    then do
+      -- TODO: show text for player that is currently selecting
+      startBox One
+      -- TODO: take first click as the player's choice
+      return ()
+    else
+      -- TODO: render the board at the current state
+      renderBoard
+        $ putPiece (One, Bee) (0, -1)
+        . putPiece (Two, Grass0) (1, 2)
+        . putPiece (One, Beetle0) (1, 0)
+        . putPiece (Two, Spider0) (0, 1)
+        . putPiece (One, Ant0) (0, 0)
+        $ emptyBoard
 
 style :: String
 style = $(do
@@ -46,3 +58,23 @@ style = $(do
   addDependentFile sassFile
   lift result
   )
+
+{- Game Widgets -}
+
+-- | When the game starts, this box will appear, prompting each player what
+-- their starting piece will be.
+startBox :: MonadWidget t m => Player -> m [Event t PieceType]
+startBox player =
+  zipWithM renderPiece' [0..]
+    [ (Bee, BeeType)
+    , (Ant0, AntType)
+    , (Grass0, GrasshopperType)
+    , (Beetle0, BeetleType)
+    , (Spider0, SpiderType)
+    ]
+  where
+    renderPiece' :: MonadWidget t m => Int -> (Piece, PieceType) -> m (Event t PieceType)
+    renderPiece' i (piece, pieceType) = do
+      let coords = toSVGCoord (i, i `div` 2)
+      (e, _) <- svg' "g" $ renderPiece coords (player, piece)
+      return $ pieceType <$ domEvent Click e
